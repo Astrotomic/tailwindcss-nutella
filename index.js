c82a6e86b33491f07583928b92e53dafb88bc03d
Tom Witkowski <dev.gummibeer@gmail.com>
2021-01-20 13:09:14 +0100

    replace outline with text-shadow

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-module.exports = require("tailwindcss/plugin")(function () {
-}, {
-    theme: {
-        extend: {
-            colors: {
-                nutella: {
-                    black: '#000000',
-                    white: '#FFFFFF',
-                    red: '#e20019',
-                    brown: '#65381B',
-                },
-            },
-            outline: {
-                'nutella-white': ['2px solid #FFFFFF', 0],
+module.exports = require("tailwindcss/plugin")(
+    function ({addUtilities, variants}) {
+        addUtilities({
+            '.text-shadown-nutella-white': {
+                'text-shadow': '2px 2px 0px #FFFFFF',
             }
-        },
-    },
-});
+        }, variants('textShadow', []));
+    }, {
+        theme: {
+            extend: {
+                colors: {
+                    nutella: {
+                        black: '#000000',
+                        white: '#FFFFFF',
+                        red: '#e20019',
+                        brown: '#65381B',
+                    },
+                },
+            }
+        }
+    }
+);
