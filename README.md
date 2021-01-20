e4aefb697dc7e5f3422cb6e7f80a39c40ac996d1
Tom Witkowski <dev.gummibeer@gmail.com>
2021-01-20 12:48:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tailwindcss-nutella
+# Tailwind CSS Nutella
+
+This [Tailwind CSS](https://tailwindcss.com/) plugin is inspired by this [tweet by @simonswiss](https://twitter.com/simonswiss/status/1351726958208450560).
+
+It adds the [Nutella]() brand colors and a matching brown to the color palette.
+
+```css
+
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = require("tailwindcss/plugin")(function () {
+}, {
+    theme: {
+        extend: {
+            colors: {
+                nutella: {
+                    black: '#000000',
+                    white: '#FFFFFF',
+                    red: '#e20019',
+                    brown: '#65381B',
+                },
+            },
+        },
+    },
+});
